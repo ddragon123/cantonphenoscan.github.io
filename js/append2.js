262fe9a161f23181e4ecdaaacbb7b13b50ca9403
ddragon123 <128720785+ddragon123@users.noreply.github.com>
2025-05-06 21:14:14 +0800

    cantonphenoscan

---
+++ js/append2.js
@@ -0,0 +1,31 @@
+$(()=>{
+	
+	// 渲染导航栏:
+	$(".banner").append(`
+		
+	<div class="nav">
+		<div class="as">
+			<img src="/cantonphenoscan.github.io/粤采千型1.png" alt="" role="presentation">
+			<a href="/cantonphenoscan.github.io/index.html">首页</a>
+			<a href="/cantonphenoscan.github.io/01社会服务.html">社会服务</a>
+			<a href="/cantonphenoscan.github.io/02农业科普.html">农业科普</a>
+			<a href="/cantonphenoscan.github.io/03用户数据.html">用户数据</a>
+			<a href="/cantonphenoscan.github.io/04AI解答.html">AI解答</a>
+			<a href="/cantonphenoscan.github.io/05交流论坛.html">交流论坛</a>
+			<a href="/cantonphenoscan.github.io/06登录.html">登录/注册</a>
+		</div>	
+	</div>
+	
+	`)
+	
+	// 渲染footer部分：
+	$(".footer").append(`
+	
+	<img src="/cantonphenoscan.github.io/粤采千型1.png" alt="" role="presentation">
+			
+	<div class="copyright">
+		小组成员：杨晨曦、何龙滨、刘吉荣、郑宇王、贾旭阳
+	</div>
+	
+	`)
+})
